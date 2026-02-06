63e56cc87926e9163f32eb5d6fa894c8caf59a03
Liam Dubay <liam.dubay@gmail.com>
2026-02-06 14:48:09 -0500

    extend to youngest stars

--- src/scripts/median_age_trends.py
+++ src/scripts/median_age_trends.py
@@ -19,7 +19,7 @@ def main(style='paper', cmap='viridis_r'):
     # Import MWM sample
     mwm_rgb = pd.read_csv(paths.data / 'MWM' / 'sample.csv')
     radius_bin_edges = np.arange(3, 15.1, 2)
-    age_bin_edges = np.arange(0.5, 11.6, 1)
+    age_bin_edges = np.arange(-0.5, 11.6, 1)
     mg_bin_edges = np.arange(-0.75, 0.76, 0.1)
 
     # Select only low-alpha, near-midplane stars
