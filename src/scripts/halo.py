c34929fa9a282a9723283d936ed1702465fad8e8
Liam Dubay <liam.dubay@gmail.com>
2026-02-11 15:37:29 -0500

    create halo summary plot

---
+++ src/scripts/halo.py
@@ -0,0 +1,229 @@
+"""
+This script plots Ce abundances in the Milky Way halo.
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from matplotlib.colors import LogNorm
+from matplotlib.ticker import MultipleLocator
+
+import paths
+from plotting import TWO_COLUMN_WIDTH
+from mwm_sample import abundance_ratio
+from colormaps import paultol
+
+DENSITY_COLORMAP = 'binary_r'
+
+
+def main(style='paper'):
+    # Get data
+    data = pd.read_csv(paths.data / 'MWM' / 'sample.csv')
+    data['mn_mg'], data['e_mn_mg'] = abundance_ratio(data, 'mn', 'mg')
+    data['al_fe'], data['e_al_fe'] = abundance_ratio(data, 'al', 'fe')
+    # Upper limits from Shetrone et al. (2015)
+    limits = data[
+        (data['ce_h'] - data['e_ce_h'] <= ce_upper_limit(data['teff'], data['snr']))
+    ]
+    data = data[
+        (data['ce_h'] - data['e_ce_h'] > ce_upper_limit(data['teff'], data['snr']))
+    ]
+    # Kinematically-selected halo
+    halo = data[data['E']/1e5 > halo_ELz_cut(data['Lz']/1e3)]
+    # Chemically-selected accreted stars
+    accreted = data[data['mn_mg'] < halo_chem_cut(data['al_fe'])]
+    # Kinematically- and chemically-selected disk stars
+    disk = data[
+        (data['E']/1e5 < halo_ELz_cut(data['Lz']/1e3)) &
+        (data['mn_mg'] > halo_chem_cut(data['al_fe']))
+    ]
+
+    # Set up figure
+    plt.style.use(paths.styles / f'{style}.mplstyle')
+    fig = plt.figure(
+        figsize=(0.8*TWO_COLUMN_WIDTH, 0.8*TWO_COLUMN_WIDTH), 
+        # constrained_layout=True
+    )
+    gs = GridSpec(2, 2, figure=fig, height_ratios=[2, 3], wspace=0.3)
+    ax0 = fig.add_subplot(gs[0,0])
+    ax1 = fig.add_subplot(gs[0,1])
+    ax2 = fig.add_subplot(gs[1,:])
+    hexbin_kwargs = dict(
+        cmap=DENSITY_COLORMAP, 
+        linewidths=0.2,
+        reduce_C_function=logsum,
+    )
+    halo_color = paultol.bright.colors[2]
+    accreted_color = paultol.bright.colors[5]
+
+    # Kinematic cut in E-Lz plane
+    ax0.set_xlabel(r'$L_z$ [$\times10^3$ kpc km s$^{-1}$]')
+    ax0.set_ylabel(r'$E$ [$\times 10^5$ km$^2$ s$^{-2}$]')
+    ax0.xaxis.set_major_locator(MultipleLocator(2))
+    ax0.xaxis.set_minor_locator(MultipleLocator(0.5))
+    ax0.yaxis.set_major_locator(MultipleLocator(0.5))
+    ax0.yaxis.set_minor_locator(MultipleLocator(0.1))
+    xlim = (-5, 5)
+    ylim = (-2.5, 0)
+    ax0.set_xlim(xlim)
+    ax0.set_ylim(ylim)
+    pc = ax0.hexbin(
+        data['Lz']/1e3, data['E']/1e5,
+        C=np.ones(data.shape[0]),
+        gridsize=50,
+        extent=[xlim[0], xlim[1], ylim[0], ylim[1]],
+        **hexbin_kwargs
+    )
+    fig.colorbar(pc, ax=ax0, label=r'$\log N$', pad=0., use_gridspec=True)
+    # Indicate boundary
+    Lz_arr = np.arange(-5, 5.1, 0.1)
+    # ax0.plot(Lz_arr, halo_ELz_cut(Lz_arr), 'w-')
+    ax0.plot(Lz_arr, halo_ELz_cut(Lz_arr), '-', color=halo_color)
+    ax0.text(
+        2, -0.5, 'Halo', 
+        fontsize=plt.rcParams['axes.titlesize'], 
+        color=halo_color
+    )
+    ax0.text(
+        -3.5, -2, 'Disk', 
+        fontsize=plt.rcParams['axes.titlesize']
+    )
+
+    # Chemical cut in Al/Fe - Mn/Mg plane
+    ax1.set_xlabel('[Al/Fe]')
+    ax1.set_ylabel('[Mn/Mg]')
+    ax1.xaxis.set_major_locator(MultipleLocator(0.5))
+    ax1.xaxis.set_minor_locator(MultipleLocator(0.1))
+    ax1.yaxis.set_major_locator(MultipleLocator(0.5))
+    ax1.yaxis.set_minor_locator(MultipleLocator(0.1))
+    xlim = (-0.8, 0.7)
+    ylim = (-1.1, 0.6)
+    ax1.set_xlim(xlim)
+    ax1.set_ylim(ylim)
+    pc = ax1.hexbin(
+        data['al_fe'], data['mn_mg'],
+        C=np.ones(data.shape[0]),
+        gridsize=50,
+        extent=[xlim[0], xlim[1], ylim[0], ylim[1]],
+        **hexbin_kwargs
+    )
+    fig.colorbar(pc, ax=ax1, label=r'$\log N$', pad=0., use_gridspec=True)
+    # Indicate boundary
+    alfe_arr = np.arange(-0.8, 0.5, 0.1)
+    # ax1.plot(alfe_arr, halo_chem_cut(alfe_arr), 'w-')
+    ax1.plot(alfe_arr, halo_chem_cut(alfe_arr), '-', color=accreted_color)
+    ax1.text(
+        0.3, 0.3, 'In situ', 
+        fontsize=plt.rcParams['axes.titlesize'], style='italic'
+    )
+    ax1.text(
+        -0.7, -0.9, 'Accreted', 
+        fontsize=plt.rcParams['axes.titlesize'], 
+        color=accreted_color,
+        bbox={'color': 'w', 'pad': 1}
+    )
+
+    # [Ce/Mg] - [Mg/H] plane
+    ax2.set_xlabel('[Mg/H]')
+    ax2.set_ylabel(r'[Ce/Mg]$_{\rm corr}$')
+    ax2.xaxis.set_major_locator(MultipleLocator(0.5))
+    ax2.xaxis.set_minor_locator(MultipleLocator(0.1))
+    ax2.yaxis.set_major_locator(MultipleLocator(0.5))
+    ax2.yaxis.set_minor_locator(MultipleLocator(0.1))
+    xlim = (-1.8, 0.6)
+    ylim = (-1.1, 1.1)
+    ax2.set_xlim(xlim)
+    ax2.set_ylim(ylim)
+    pc = ax2.hexbin(
+        disk['mg_h'], disk['ce_mg_corr'],
+        C=np.ones(disk.shape[0]),
+        gridsize=(50, 18),
+        extent=[xlim[0], xlim[1], ylim[0], ylim[1]],
+        **hexbin_kwargs
+    )
+    fig.colorbar(
+        pc, ax=ax2, label=r'$\log N$ (disk)', 
+        pad=0., fraction=0.07, aspect=30, 
+        use_gridspec=True
+    )
+    # Plot kinematically-selected halo stars
+    scatter_kwargs = dict(
+        s=5, rasterized=True, #edgecolors='w', linewidths=0.5,
+    )
+    ax2.scatter(
+        halo['mg_h'], halo['ce_mg_corr'], 
+        c=halo_color, marker='o', 
+        label='Kinematic halo',
+        **scatter_kwargs
+    )
+    # Rolling median
+    sorted_halo = halo.sort_values('mg_h')[['mg_h', 'ce_mg_corr']]
+    rolling_halo = sorted_halo.rolling(
+        100, min_periods=100, step=10, on='mg_h', center=True
+    ).median()
+    ax2.plot(rolling_halo['mg_h'], rolling_halo['ce_mg_corr'], 'w-', linewidth=2)
+    ax2.plot(rolling_halo['mg_h'], rolling_halo['ce_mg_corr'], '-', color=halo_color)
+    # Plot chemically-selected accreted stars
+    ax2.scatter(
+        accreted['mg_h'], accreted['ce_mg_corr'],
+        c=accreted_color, marker='D', 
+        label='Chemical accreted',
+        **scatter_kwargs
+    )
+    # Rolling median
+    sorted_accreted = accreted.sort_values('mg_h')[['mg_h', 'ce_mg_corr']]
+    rolling_accreted = sorted_accreted.rolling(
+        100, min_periods=100, step=10, on='mg_h', center=True
+    ).median()
+    ax2.plot(rolling_accreted['mg_h'], rolling_accreted['ce_mg_corr'], 'w-', linewidth=2)
+    ax2.plot(rolling_accreted['mg_h'], rolling_accreted['ce_mg_corr'], '-', color=accreted_color)
+    # Indicate grid edges
+    mgh_arr = np.arange(-2.5, 1.25, 0.25)
+    ax2.plot(mgh_arr, -2.1 - mgh_arr, 'k:', label='Flag edge') # edge of stars flagged bad
+    ax2.plot(mgh_arr, -1.6 - mgh_arr, color='gray', ls=':', label='Upper limits') # indicates region of upper limits (manual)
+    ax2.plot(mgh_arr, 0.9 - mgh_arr, 'k:') # edge of stars flagged bad
+    ax2.legend(loc='upper left', frameon=True)
+    # Indicate median abundance error
+    ax2.errorbar(
+        -1.7, -0.9, 
+        xerr=data['e_mg_h'].median(), 
+        yerr=data['e_ce_mg'].median(),
+        c='k', capsize=0
+    )
+
+    plt.subplots_adjust(bottom=0.08, top=0.96, left=0.08, right=0.92)
+    plt.savefig(paths.figures / 'halo')
+    plt.close()
+
+
+def logsum(x):
+    """
+    Log of the sum of an array.
+    """
+    return np.log10(np.sum(x))
+
+
+def halo_ELz_cut(Lz):
+    """
+    Arbitrary halo cut - find a better one in the literature
+    """
+    return -0.55 - np.exp(Lz / 2.5)
+
+
+def halo_chem_cut(alfe):
+    """
+    Chemical cut in [Mn/Mg]-[Al/Fe] plane to select accreted stars.
+    """
+    return np.where(alfe > -0.2, -0.6-2*alfe, -0.2)
+
+
+def ce_upper_limit(teff, snr):
+    """
+    Ce abundance upper limits based on Teff and S/N from Shetrone et al. (2025).
+    """
+    return -0.93 + 0.51*(teff/1000) - 1.84*(np.log10(snr)) + 0.174*(np.log10(snr))**2
+
+
+if __name__ == '__main__':
+    main()
