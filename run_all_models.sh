961d1041e7795a72e1f82fbdde1236c0d79d85eb
Liam Dubay <liam.dubay@gmail.com>
2026-03-18 16:56:34 -0400

    update model sets

--- run_all_models.sh
+++ run_all_models.sh
@@ -4,34 +4,78 @@
 cd ./src/scripts/
 
 NSTARS=2
-EVOLUTION=insideout
 
-NAME="fiducial"
+NAME="yZ1-c11-fiducial"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=fiducial --evolution=$EVOLUTION
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --agb-yields=cristallo11
 echo ""
 
-NAME="low-sfe"
+NAME="yZ1-c11-lowsfe"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --sfe-factor=2
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --agb-yields=cristallo11 --sfe-factor=2
 echo ""
 
-NAME="agb-mscale"
+NAME="yZ1-c11-mscale"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=mscale
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --agb-yields=cristallo11 --agb-mscale=0.5
 echo ""
 
-NAME="agb-Zscale"
+NAME="yZ1-c11-Zscale"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=Zscale
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --agb-yields=cristallo11 --agb-Zscale=2
 echo ""
 
-NAME="agb-only"
+NAME="yZ1-c11-onlyagb"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=onlyagb
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --agb-yields=cristallo11 --r-channel=none
 echo ""
 
-NAME="lateburst"
+NAME="yZ1-c11-rdelay-plaw"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --evolution=lateburst
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --agb-yields=cristallo11 --r-channel=sneia --RIa=powerlaw --minimum-delay=0.01
+echo ""
+
+NAME="yZ1-c11-lateburst"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --agb-yields=cristallo11 --evolution=lateburst
+echo ""
+
+NAME="yZ2-c11-x2"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --agb-yields=cristallo11 --agb-amp=2
+echo ""
+
+NAME="yZ2-k16-fiducial"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --agb-yields=karakas16
+echo ""
+
+NAME="yZ2-k16-lowsfe"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --agb-yields=karakas16 --sfe-factor=2
+echo ""
+
+NAME="yZ2-k16-mscale"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --agb-yields=karakas16 --agb-mscale=0.5
+echo ""
+
+NAME="yZ2-k16-Zscale"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --agb-yields=karakas16 --agb-Zscale=2
+echo ""
+
+NAME="yZ2-k16-onlyagb"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --agb-yields=karakas16 --r-channel=none
+echo ""
+
+NAME="yZ2-k16-rdelay-plaw"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --agb-yields=karakas16 --r-channel=sneia --RIa=powerlaw --minimum-delay=0.01
+echo ""
+
+NAME="yZ2-k16-lateburst"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --agb-yields=karakas16 --evolution=lateburst
 echo ""
