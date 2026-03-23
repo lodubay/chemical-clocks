4e3708d17a4140fe23a1734c439a275b470d9a93
Liam Dubay <liam.dubay@gmail.com>
2026-03-23 18:20:19 -0400

    update best model plot

--- src/scripts/model_cemg_mgh.py
+++ src/scripts/model_cemg_mgh.py
@@ -11,12 +11,14 @@ from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MultipleLocator
 
 from multizone_stars import MultizoneStars
-from plotting import setup_hayden_plot, iterate_rz_bins
+from plotting import TWO_COLUMN_WIDTH, insert_colorbar_axes
 from utils import plot_gas_abundance
 from contours import plot_kde2D_contours
 import paths
 
-OUTPUT_NAME = 'agb-mscale/diskmodel'
+OUTPUT_NAME = 'yZ2-k16-mscale/diskmodel'
+RBINS = [(3, 5), (7, 9), (11, 13)]
+ZLIM = (0, 2)
 
 
 def main(style='paper', cmap='Spectral_r'):
@@ -24,10 +26,13 @@ def main(style='paper', cmap='Spectral_r'):
     sample = pd.read_csv(paths.data / 'MWM' / 'sample.csv')
     # Set up figure
     plt.style.use(paths.styles / f'{style}.mplstyle')
-    fig, axs = setup_hayden_plot()
-    # Enlarge for colorbar
-    figsize = fig.get_size_inches()
-    fig.set_size_inches((figsize[0], figsize[1] * 1.25))
+    fig, axs = plt.subplots(
+        1, 3, 
+        figsize=(TWO_COLUMN_WIDTH, 0.33*TWO_COLUMN_WIDTH),
+        sharex=True, sharey=True,
+        gridspec_kw={'wspace': 0.}
+    )
+    # cax = insert_colorbar_axes(fig, pad=0.02)
     age_bounds = np.arange(0, 12.1, 2)
     cmap = plt.get_cmap(cmap)
     cbar = fig.colorbar(
@@ -36,46 +41,46 @@ def main(style='paper', cmap='Spectral_r'):
             cmap
         ), 
         ax=axs,
-        shrink=0.6, 
-        aspect=30, 
-        fraction=0.1, 
-        pad=0.1,
-        orientation='horizontal',
+        # shrink=0.6, 
+        aspect=15, 
+        fraction=0.05, 
+        pad=0.02,
+        orientation='vertical',
         label='Age [Gyr]'
     )
     # Plot multizone output
     mzs = MultizoneStars.from_output(OUTPUT_NAME)
     mzs.model_uncertainty(sample, inplace=True)
-    for i, j, zlim, rlim in iterate_rz_bins():
-        ax = axs[i,j]
-        mzs_subset = mzs.region(rlim, zlim)
+    for i, rlim in enumerate(RBINS):
+        ax = axs[i]
+        mzs_subset = mzs.region(rlim, absz_lim=ZLIM)
         mzs_subset.scatter_plot(
             ax, '[mg/h]', '[ce/mg]', color='age',
-            cmap=cbar.cmap, norm=cbar.norm
+            cmap=cbar.cmap, norm=cbar.norm,
+            markersize=0.5,
         )
         plot_gas_abundance(ax, mzs_subset, '[mg/h]', '[ce/mg]', c='k', ls='--')
         # Plot MWM data contours
         sample_subset = sample[
             (sample['Rg'] >= rlim[0]) &
             (sample['Rg'] < rlim[1]) &
-            (sample['z_max'] >= zlim[0]) &
-            (sample['z_max'] < zlim[1])
+            (sample['z_max'] >= ZLIM[0]) &
+            (sample['z_max'] < ZLIM[1])
         ]
         plot_kde2D_contours(ax, sample_subset, 'mg_h', 'ce_mg_corr')
     
     # Format axes
-    axs[0,0].set_xlim((-0.8, 0.6))
-    axs[0,0].set_ylim((-0.8, 0.8))
-    for ax in axs[-1]:
+    axs[0].set_xlim((-0.8, 0.6))
+    axs[0].set_ylim((-0.8, 0.8))
+    for ax in axs:
         ax.set_xlabel('[Mg/H]')
-    for ax in axs[:,0]:
-        ax.set_ylabel('[Ce/Mg]', labelpad=-2)
+    axs[0].set_ylabel('[Ce/Mg]', labelpad=-2)
     # Set x-axis ticks
-    axs[0,0].xaxis.set_major_locator(MultipleLocator(0.5))
-    axs[0,0].xaxis.set_minor_locator(MultipleLocator(0.1))
+    axs[0].xaxis.set_major_locator(MultipleLocator(0.5))
+    axs[0].xaxis.set_minor_locator(MultipleLocator(0.1))
     # Set y-axis ticks
-    axs[0,0].yaxis.set_major_locator(MultipleLocator(0.5))
-    axs[0,0].yaxis.set_minor_locator(MultipleLocator(0.1))
+    axs[0].yaxis.set_major_locator(MultipleLocator(0.5))
+    axs[0].yaxis.set_minor_locator(MultipleLocator(0.1))
     
     plt.savefig(paths.figures / 'model_cemg_mgh.pdf')
     plt.close()
