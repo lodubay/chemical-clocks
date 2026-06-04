0c11fa9fbc2eba99f8acbcda89b6af47a1d17d66
Liam Dubay <liam.dubay@gmail.com>
2026-06-04 13:14:43 -0400

    combine residual abundance code and explainer plot

--- src/scripts/residual_abundances.py
+++ src/scripts/residual_abundances.py
@@ -1,142 +1,155 @@
 """
-Plot metallicity-corrected residual Ce abundances as a function of
-position in the Galaxy.
+This script contains code for calculating residual abundances. If run as a 
+script, it makes a plot explaining the calculation of the residual Ce abundance.
 """
+import argparse
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
 from utils import binned_quantiles, sample_rows
-from plotting import colored_text_legend, setup_hayden_plot, iterate_rz_bins
+from plotting import colored_text_legend, iterate_rz_bins, ONE_COLUMN_WIDTH
 from colormaps import paultol
 import paths
 
 RBINS = [(3, 5), (5, 7), (7, 9), (9, 11), (11, 13)] # left to right
 ZBINS = [(1, 2), (0.5, 1), (0, 0.5)] # top to bottom
-ALPHA_BUFFER = 0.02 # dex, buffer around the [Mg/Fe] dividing line
-# SAMPLE_FRACTION = 0.25 # fraction of stars to plot in each panel
-SAMPLE_SIZE = 1000 # number of stars to plot in each panel, randomly selected
+SAMPLE_FRACTION = 0.25 # fraction of stars to plot in each panel
 
-def main(style='paper'):
-    plt.style.use(paths.styles / f'{style}.mplstyle')
+def main(style='paper', sample_fraction=SAMPLE_FRACTION):
     # Import MWM sample
     mwm_rgb = pd.read_csv(paths.data / 'sample.csv')
-    mwm_res = residual_abundances(mwm_rgb)
-    mwm_res_ages = mwm_res[mwm_res['good_age']].copy() # Good ages only
-
+    # Solar neighborhood sample
+    mwm_rgb_local = mwm_rgb[
+        (mwm_rgb['Rg'] >= 7) &
+        (mwm_rgb['Rg'] < 9) &
+        (mwm_rgb['z_max'] < 0.5)
+    ].copy()
+    # Calculate residual abundances (Solar neighborhood only)
+    mwm_rgb_local = residual_abundances(
+        mwm_rgb_local, 
+        col='ce_h_corr', 
+        newcol='delta_ce_h',
+        rbins=[(7, 9)],
+        zbins=[(0, 0.5)]
+    )
+    # Scatter plot random sample of points
+    sample = sample_rows(
+        mwm_rgb_local, 
+        int(sample_fraction * mwm_rgb_local.shape[0])
+    )
+    mg_bin_edges = np.arange(-0.75, 0.56, 0.1)
     age_bin_edges = np.arange(0.5, 11.6, 1)
 
-    # Solar neighborhood sample
-    local_low_alpha = mwm_res_ages[
-        (mwm_res_ages['Rg'] >= 7) &
-        (mwm_res_ages['Rg'] < 9) &
-        (mwm_res_ages['z_max'] < 0.5) &
-        (mwm_res_ages['low_alpha'])
-    ].copy()
-    local_high_alpha = mwm_res_ages[
-        (mwm_res_ages['Rg'] >= 7) &
-        (mwm_res_ages['Rg'] < 9) &
-        (mwm_res_ages['z_max'] < 0.5) &
-        (mwm_res_ages['high_alpha'])
-    ].copy()
-    # Calculate median trend with age (only stars with good ages)
-    local_low_alpha_age_medians = binned_quantiles(
-        local_low_alpha, 'delta_ce_h', 'age',
-        q=0.5, bin_edges=age_bin_edges, min_count=10
+    # Set up figure
+    plt.style.use(paths.styles / f'{style}.mplstyle')
+    fig, axs = plt.subplots(2, 2, 
+        figsize=(ONE_COLUMN_WIDTH, ONE_COLUMN_WIDTH),
+        sharex='col', sharey='row', 
+        gridspec_kw={'hspace': 0, 'wspace': 0}
     )
-    local_high_alpha_age_medians = binned_quantiles(
-        local_high_alpha, 'delta_ce_h', 'age',
-        q=0.5, bin_edges=age_bin_edges, min_count=10
-    )
-
-    fig, axs = setup_hayden_plot(rbins=RBINS, zbins=ZBINS)
-    plt.subplots_adjust(left=0.1, right=0.95, bottom=0.1, top=0.95)
     # scatterplot style arguments
     kwargs = dict(s=1, marker='.', rasterized=True, edgecolor='none')
-    high_alpha_color = paultol.highcontrast.colors[2]
-    low_alpha_color = paultol.highcontrast.colors[0]
+    # Label each panel
+    labels = ['(a)', '(b)', '(c)', '(d)']
+    for i, ax in enumerate(axs.flatten()):
+        ax.set_title(labels[i], y=0.93, x=0.07, ha='left', va='top', pad=0)
 
-    for i, j, zlim, rlim in iterate_rz_bins(rbins=RBINS, zbins=ZBINS):
-        subset = mwm_res_ages[
-            (mwm_res_ages['Rg'] >= rlim[0]) &
-            (mwm_res_ages['Rg'] < rlim[1]) &
-            (mwm_res_ages['z_max'] >= zlim[0]) &
-            (mwm_res_ages['z_max'] < zlim[1])
-        ]
-        low_alpha = subset[subset['low_alpha']].copy()
-        high_alpha = subset[subset['high_alpha']].copy()
-        # Select random sample of stars for scatter plot
-        sample = sample_rows(subset, SAMPLE_SIZE)
-        low_alpha_sample = sample[sample['low_alpha']]
-        high_alpha_sample = sample[sample['high_alpha']]
-        if low_alpha.shape[0] >= 100:
-            # Scatter plot random sample of points
-            axs[i,j].scatter(
-                low_alpha.loc[low_alpha_sample.index, 'age'], 
-                low_alpha.loc[low_alpha_sample.index, 'delta_ce_h'],
-                c=low_alpha_color, zorder=2, **kwargs
-            )
-            # Plot median trend with age
-            age_medians = binned_quantiles(
-                low_alpha, 'delta_ce_h', 'age',
-                q=0.5, bin_edges=age_bin_edges, min_count=10
-            )
-            axs[i,j].plot(
-                *age_medians, '.-', color=low_alpha_color, zorder=6,
-                label='High-Ia'
-            )
-        if high_alpha.shape[0] >= 100:
-            # Scatter plot random sample of points
-            axs[i,j].scatter(
-                high_alpha.loc[high_alpha_sample.index, 'age'], 
-                high_alpha.loc[high_alpha_sample.index, 'delta_ce_h'],
-                c=high_alpha_color, zorder=1, **kwargs
-            )
-            # Plot median trend with age
-            age_medians = binned_quantiles(
-                high_alpha, 'delta_ce_h', 'age',
-                q=0.5, bin_edges=age_bin_edges, min_count=10
-            )
-            axs[i,j].plot(
-                *age_medians, '.-', color=high_alpha_color, zorder=5,
-                label='Low-Ia'
-            )
-        # Plot local low and high-alpha trends for comparison
-        axs[i,j].plot(
-            *local_low_alpha_age_medians, 
-            linestyle='--', color=low_alpha_color, zorder=4,
+    # Loop through low- and high-alpha populations
+    pops = ['low_alpha', 'high_alpha']
+    colors = [paultol.highcontrast.colors[0], paultol.highcontrast.colors[2]]
+    labels = ['High-Ia', 'Low-Ia']
+    formats = ['s-', 'o-']
+    ms = 3
+    for pop, color, label, fmt in zip(pops, colors, labels, formats):
+        local_pop = mwm_rgb_local[mwm_rgb_local[pop]]
+        sample_pop = sample[sample[pop]]
+        # Plot [Mg/H] vs [Ce/H]
+        axs[0,0].scatter(
+            sample_pop['mg_h'], sample_pop['ce_h_corr'], 
+            c=color, **kwargs
         )
-        axs[i,j].plot(
-            *local_high_alpha_age_medians, 
-            linestyle='--', color=high_alpha_color, zorder=3,
+        # Plot median trend with [Mg/H]
+        local_pop_medians = binned_quantiles(
+            local_pop, 'ce_h_corr', 'mg_h', 
+            q=0.5, bin_edges=mg_bin_edges, min_count=10
         )
-        # Horizontal line for reference
-        axs[i,j].plot([0, 12], [0, 0], linestyle=':', color='gray', zorder=0)
-    # Indicate median abundance errors
-    age_err_low = np.median(mwm_res_ages['age'] - mwm_res_ages['e_n_age'])
-    age_err_high = np.median(mwm_res_ages['e_p_age'] - mwm_res_ages['age'])
-    med_abund_err = mwm_res_ages['e_ce_h'].median()
-    axs[0,0].errorbar(
-        3, -0.4, 
-        xerr=[[age_err_low], [age_err_high]], 
-        yerr=med_abund_err, 
-        c='gray', capsize=0, #elinewidth=0.5,
-    )
+        axs[0,0].plot(
+            *local_pop_medians, 
+            fmt, color=color, label=label, ms=ms
+        )
+        # Plot [Ce/H] residuals vs [Mg/H]
+        axs[1,0].scatter(
+            sample_pop['mg_h'], sample_pop['delta_ce_h'],
+            c=color, **kwargs
+        )
+        # Plot median and 1-sigma bands
+        for q, ls in zip([0.16, 0.5, 0.84], ['--', '-', '--']):
+            local_pop_quantile = binned_quantiles(
+                local_pop, 'delta_ce_h', 'mg_h',
+                q=q, bin_edges=mg_bin_edges, min_count=10
+            )
+            axs[1,0].plot(*local_pop_quantile, ls, color=color, zorder=6)
+        # Plot [Ce/H] vs age
+        sample_pop_ages = sample_pop[sample_pop['good_age']]
+        axs[0,1].scatter(
+            sample_pop_ages['age'], sample_pop_ages['ce_h_corr'],
+            c=color, **kwargs
+        )
+        # Plot median trends with age
+        local_pop_ages = local_pop[local_pop['good_age']]
+        pop_age_medians = binned_quantiles(
+            local_pop_ages, 'ce_h_corr', 'age',
+            q=0.5, bin_edges=age_bin_edges, min_count=10
+        )
+        axs[0,1].plot(*pop_age_medians, fmt, color=color, zorder=6, ms=ms)
+        # Plot [Ce/H] residuals vs age
+        axs[1,1].scatter(
+            sample_pop_ages['age'], sample_pop_ages['delta_ce_h'],
+            c=color, **kwargs
+        )
+        # Plot median trends with age
+        pop_res_age_medians = binned_quantiles(
+            local_pop_ages, 'delta_ce_h', 'age',
+            q=0.5, bin_edges=age_bin_edges, min_count=10
+        )
+        axs[1,1].plot(*pop_res_age_medians, fmt, color=color, zorder=6, ms=ms)
+        
+    # Horizontal lines for reference
+    axs[1,0].plot([-0.7, 0.6], [0, 0], linestyle=':', color='gray', zorder=5)
+    axs[1,1].plot([-1, 12], [0, 0], linestyle=':', color='gray', zorder=5)
 
-    # Format axes
-    axs[0,0].set_xlim((0, 12))
-    axs[0,0].set_ylim((-0.6, 0.6))
-    axs[0,0].xaxis.set_major_locator(MultipleLocator(5))
-    axs[0,0].xaxis.set_minor_locator(MultipleLocator(1))
+    # Axes labels
+    axs[0,0].set_ylabel('[Ce/H]')
+    axs[1,0].set_ylabel(r'$\Delta$[Ce/H]')
+    axs[1,0].set_xlabel('[Mg/H]')
+    axs[1,1].set_xlabel('Age [Gyr]')
+
+    # Axes limits
+    axs[0,0].set_xlim((-0.7, 0.6))
+    axs[0,0].set_ylim((-0.8, 0.8))
+    axs[1,0].set_ylim((-0.8, 0.8))
+    axs[0,1].set_xlim((-1, 12))
+
+    # Axes ticks
+    axs[0,0].xaxis.set_major_locator(MultipleLocator(0.5))
+    axs[0,0].xaxis.set_minor_locator(MultipleLocator(0.1))
     axs[0,0].yaxis.set_major_locator(MultipleLocator(0.5))
     axs[0,0].yaxis.set_minor_locator(MultipleLocator(0.1))
-    for ax in axs[-1,:]:
-        ax.set_xlabel('Age [Gyr]')
-    for ax in axs[:,0]:
-        ax.set_ylabel(r'$\Delta$[Ce/H]')
-    # Text-only lengend
-    leg = colored_text_legend(axs[0,0], loc='upper left')
+    axs[1,0].yaxis.set_major_locator(MultipleLocator(0.5))
+    axs[1,0].yaxis.set_minor_locator(MultipleLocator(0.1))
+    axs[0,1].xaxis.set_major_locator(MultipleLocator(5))
+    axs[0,1].xaxis.set_minor_locator(MultipleLocator(1))
+
+    leg = colored_text_legend(
+        axs[0,0], 
+        loc='lower right',
+        frameon=True,
+        framealpha=1,
+        edgecolor='none'
+    )
 
     plt.savefig(paths.figures / 'residual_abundances')
 
@@ -210,4 +223,18 @@ def residual_abundances(
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Explainer plot for the residual abundance calculation.'
+    )
+    parser.add_argument('--style',
+        choices=('paper', 'poster'),
+        default='paper',
+        help='Plot style to use (default: paper).'
+    )
+    parser.add_argument('--sample-fraction',
+        type='float',
+        default=SAMPLE_FRACTION,
+        help='Fraction of stars to randomly sample in each panel.'
+    )
+    args = parser.parse_args()
+    main(**vars(args))
